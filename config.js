7f4cf173c87bb0b31c81f7f5c54cec269d0c453f
kuzuri <manhluongd@gmail.com>
2015-12-14 19:23:05 +0700

    config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+/** Dima ComingSoon settings **/
+
+/* ------------------ The countdown settings --------------------------------- */
+// Formate of date: MOUNTH DAY, YEAR HOURS:MINUTES:SECONDS
+// time to count down to
+var end_date = 'April, 30 2014 12:00:00';
+
+// Target time in Los Angeles, USA (GMT -08:00): 
+// If timezone is left at null it defaults to the timezone on the client.
+var tm_zone = -4;
+
+// you can have a new page loaded automatically on expiry.
+// [url OR null] e.g. "http://google.com";
+var expiry_goto = null;
+
+// Color scheme
+
+var strokes= ['rgba(241,205,33,1)',
+			  'rgba(255,255,255,1)',
+			  'rgba(241,205,33,1)',
+			  'rgba(255,255,255,1)'];
+
